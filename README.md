0bbab4d7bd7f9da711bc16c05626540e5cd76840
yubaoquan <hzyubaoquan@corp.netease.com>
2017-10-14 16:55:52 +0800

    Fix popup button icon position

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Fix the position bug of google translate extension.
 
 Just override the `position` property of class `jfk-bubble`, so the position will change from `absolute` to `fixed`, and you will see the translate bubble again.
+
+Also fix the position of `pop-up icon`, which is for user to click to call the popup panel.
--- myscript.js
+++ myscript.js
@@ -1,8 +1,16 @@
 
 fixTranslateBubble()
-
+function fixPopupIcon() {
+    const buttonIcon = document.querySelector('#gtx-trans')
+    if (buttonIcon) {
+        buttonIcon.style.position = 'fixed'
+    }
+}
 function fixTranslateBubble() {
     const style = document.createElement('style')
     style.innerText = '.jfk-bubble { position: fixed !important; }'
     document.body.append(style)
+    document.body.addEventListener('click', () => {
+        setTimeout(fixPopupIcon, 500)
+    })
 }
